ad17f819e11d74a48236634181455b28343e3422
i-chandanpatel <152427076+i-chandanpatel@users.noreply.github.com>
2024-10-22 12:42:51 +0530

    Update MaximumSubarraySum.cpp

--- MaximumSubarraySum.cpp
+++ MaximumSubarraySum.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#inxlude<algorithm>
 using namespace std;
 
 int maxSubarraySum(int arr[], int n){
@@ -16,9 +17,41 @@ int maxSubarraySum(int arr[], int n){
 	return maxSum;
 }
 
+//Using Prefix Array
+int maxSubarraySum_Prefix(int arr[], int n){
+	int currSum=0;
+	int maxSum=INT_MIN;
+	int prefix[n];
+	prefix[0]=arr[0];
+	for(int i=1; i<n; i++){
+		prefix[i]=prefix[i-1]+arr[i];
+	}
+	for(int i=0; i<n; i++){
+		for(int j=0; j<n; j++){
+			currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
+			
+			if(currSum>maxSum)	maxSum=currSum;
+		}
+	}
+	return maxSum;
+}
+
+//Using Kadan's Algorihm
+int maxSubarraySum_Kadan(int arr[], int n){
+	int maxSum=INT_MIN, currSum=0;
+	for(int i=0; i<n; i++){
+		currSum+=arr[i];
+		if(currSum<0)	currSum=0;
+		
+		maxSum=max(currSum,maxSum);
+	}
+	return maxSum;
+}
 int main(){
 	int n=6;
 	int arr[n]={1,2,3,-4,5,11};
 	cout<<maxSubarraySum(arr,n);
+//	cout<<maxSubarraySum_Prefix(arr,n);
+//	cout<<maxSubarraySum_Kadan(arr,n);
 	return 0;
 }
