3e22888e97ca0d87a93740bd5940224fd501698f
i-chandanpatel <152427076+i-chandanpatel@users.noreply.github.com>
2024-10-22 22:15:58 +0530

    Add files via upload

---
+++ Strings.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+//Strings in C++
+//Header File: #include <string>
+
+//Characteristics
+//Dynamic Size, Mutable
+//Common Functions
+
+//Creation
+//string str1 = "Hello";
+//Size and Capacity
+//str.size() or str.length() // Returns number of characters
+//str.capacity() // Returns size of allocated storage
+
+//Accessing Characters
+
+//str[index] or str.at(index) // Access character at index
+
+//Modification
+
+//str.append(" World") or str += " World"
+//str.insert(position, "inserted")
+//str.erase(position, count)
+//str.replace(start, length, "replacement")
+
+//Searching
+
+//str.find("substring") // Returns index of first occurrence or   string::npos
+//str.rfind("substring") // Returns index of last occurrence
+//str.find_first_of("chars") // Finds first occurrence of any character from chars
+
+//Substrings
+
+//str.substr(start, length) // Extracts a substring
+
+//Comparison
+
+//str1.compare(str2) // Lexicographical comparison
+//str1 == str2, str1 != str2, etc. // Comparison operators
+
+//Conversion
+
+//stoi(str) // Convert string to integer
+//stof(str) // Convert string to float
+//to_string(value) // Convert number to string
+	return 0;
+}
