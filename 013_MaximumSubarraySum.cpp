4bbea07fb7b6835cbcc02f1a1237d5e6f50b1435
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-04-25 10:04:21 +0530

    Update 013_MaximumSubarraySum.cpp

--- 013_MaximumSubarraySum.cpp
+++ 013_MaximumSubarraySum.cpp
@@ -1,26 +1,26 @@
-#include<iostream>
-#inxlude<algorithm>
+#include <iostream>
+#include <algorithm>
+#include <climits>
 using namespace std;
 
-int maxSubarraySum(int arr[], int n){
-	int currSum=0;
-	int maxSum=INT_MIN;
-	for(int i=0; i<n; i++){
-		for(int j=0; j<n; j++){
-			currSum=0;
-			for(int k=i; k<=j; k++){
-				currSum+=arr[k];
-			}
-			if(currSum>maxSum)	maxSum=currSum;
-		}
-	}
-	return maxSum;
+// Brute Force (O(n^3))
+int maxSubarraySum(int arr[], int n) {
+    int currSum = 0, maxSum = INT_MIN;
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            currSum = 0;
+            for (int k = i; k <= j; k++) {
+                currSum += arr[k];
+            }
+            maxSum = max(maxSum, currSum);
+        }
+    }
+    return maxSum;
 }
 
-
+// Optimized (O(n^2))
 int maxSubarraySum2(int arr[], int n) {
     int maxSum = INT_MIN;
-
     for (int st = 0; st < n; st++) {
         int currSum = 0;
         for (int end = st; end < n; end++) {
@@ -28,39 +28,35 @@ int maxSubarraySum2(int arr[], int n) {
             maxSum = max(currSum, maxSum);
         }
     }
-
     return maxSum;
 }
 
-//Using Prefix Array
-int maxSubarraySum_Prefix(int arr[], int n){
-	int currSum=0;
-	int maxSum=INT_MIN;
-	int prefix[n];
-	prefix[0]=arr[0];
-	for(int i=1; i<n; i++){
-		prefix[i]=prefix[i-1]+arr[i];
-	}
-	for(int i=0; i<n; i++){
-		for(int j=0; j<n; j++){
-			currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i-1];
-			
-			if(currSum>maxSum)	maxSum=currSum;
-		}
-	}
-	return maxSum;
+// Using Prefix Array (O(n^2))
+int maxSubarraySum_Prefix(int arr[], int n) {
+    int currSum = 0, maxSum = INT_MIN;
+    int prefix[n];
+    prefix[0] = arr[0];
+    for (int i = 1; i < n; i++) {
+        prefix[i] = prefix[i - 1] + arr[i];
+    }
+    for (int i = 0; i < n; i++) {
+        for (int j = i; j < n; j++) {
+            currSum = i == 0 ? prefix[j] : prefix[j] - prefix[i - 1];
+            maxSum = max(maxSum, currSum);
+        }
+    }
+    return maxSum;
 }
 
-//Using Kadan's Algorihm
-int maxSubarraySum_Kadan(int arr[], int n){
-	int maxSum=INT_MIN, currSum=0;
-	for(int i=0; i<n; i++){
-		currSum+=arr[i];
-		if(currSum<0)	currSum=0;
-		
-		maxSum=max(currSum,maxSum);
-	}
-	return maxSum;
+// Kadane's Algorithm (O(n))
+int maxSubarraySum_Kadan(int arr[], int n) {
+    int maxSum = arr[0];
+    int currSum = arr[0];
+    for (int i = 1; i < n; i++) {
+        currSum = max(arr[i], currSum + arr[i]);
+        maxSum = max(maxSum, currSum);
+    }
+    return maxSum;
 }
 int main(){
 	int n=6;
