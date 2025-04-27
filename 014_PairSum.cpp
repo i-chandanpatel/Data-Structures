911696f298c2814accd08226b8a78f5127312480
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-04-27 22:18:29 +0530

    Update 014_PairSum.cpp

--- 014_PairSum.cpp
+++ 014_PairSum.cpp
@@ -20,6 +20,42 @@ void pairSumBruteForce(const vector<int>& nums, int target) {
     }
 }
 
+vector<int> pairSum(vector<int> nums, int target) {
+    vector<int> ans;
+    int n = nums.size();
+
+    for (int i = 0; i < n; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (nums[i] + nums[j] == target) {
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }
+        }
+    }
+
+    return ans;
+}
+
+//Paur Sum on sorted Array
+vector<int>pairSumOptimal(vector<int> nums, target){
+    vector<int> ans;
+    int n = nums.size();
+
+    int i=0, j=n, pairSum;
+    while(i<j){
+        pairSum=arr[i]+arr[j];
+            if (pairSum == target) {
+                ans.push_back(i);
+                ans.push_back(j);
+                return ans;
+            }else if(pairSum>target) j--
+            else if(pairSum<target) i++
+    }
+
+    return ans;
+}
+
 int main() {
     vector<int> arr = {2, 7, 4, 5, 1, 3};
     int target = 6;
