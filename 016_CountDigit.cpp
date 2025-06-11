48e72001087180463f3856ebf64c98fb1c2d8666
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-06-11 12:13:48 +0530

    Create 016_CountDigit.cpp

---
+++ 016_CountDigit.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int count(int n){
+int cnt=0;
+  while(n>0){
+    n%=10;
+    n/=10;
+    cnt++;
+  }
+  return cnt;
+}
+
+int count2(int n){
+  int cnt=(int)(log10(n)+1)
+  return cnt;
+}
+int main(){
+   int num=374747;
+   cout<<count(num);
+   //cout<<count(num);
+}
