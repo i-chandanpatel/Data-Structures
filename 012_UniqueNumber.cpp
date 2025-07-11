b9ab30f819cb0247cbdcc785c937da368d1fab8f
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-07-11 15:31:50 +0530

    Update and rename 012_SingleNumber.cpp to 012_UniqueNumber.cpp

--- 012_UniqueNumber.cpp
+++ 012_UniqueNumber.cpp
@@ -12,9 +12,21 @@ int singleNumber(const vector<int>& nums) {
     return ans;
 }
 
+int uniqueElement(int arr[], int n){
+	int res=0;
+	for(int i=0; i<n; i++){
+		res=res^arr[i];
+	}
+	return res;
+}
 int main() {
     vector<int> nums = {4, 1, 2, 1, 2};
     cout<<singleNumber(nums);
+
+    // int arr[]={1,1,5,6,4,4,5};
+	// int n=sizeof(arr)/sizeof(arr[0]);
+	// cout<<uniqueElement(arr,n);
+
     return 0;
 }
 
