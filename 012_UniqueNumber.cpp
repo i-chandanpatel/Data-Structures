400d628ef949524c9df3efdcb1cf4d1974aa3943
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-07-19 11:16:42 +0530

    Update 012_UniqueNumber.cpp

--- 012_UniqueNumber.cpp
+++ 012_UniqueNumber.cpp
@@ -19,13 +19,31 @@ int uniqueElement(int arr[], int n){
 	}
 	return res;
 }
+
+//All unique Brute Force
+void allUnique(int arr[], int n){
+	bool isUnique=true;
+	for(int i=0; i<n; i++){
+		isUnique=true;
+		for(int j=0; j<n; j++){
+			if(i!=j && arr[i]==arr[j]){
+				isUnique=false;
+				break;
+			}
+		}
+		if(isUnique){
+			cout<<arr[i]<<endl;
+		}
+	}
+}
+
 int main() {
     vector<int> nums = {4, 1, 2, 1, 2};
     cout<<uniqueNumVec(nums);
 
     // int arr[]={1,1,5,6,4,4,5};
-	// int n=sizeof(arr)/sizeof(arr[0]);
-	// cout<<uniqueElement(arr,n);
+    // int n=sizeof(arr)/sizeof(arr[0]);
+    // cout<<uniqueElement(arr,n);
 
     return 0;
 }
