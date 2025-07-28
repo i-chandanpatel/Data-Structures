9a370167951cf051837960094c09b2f1e67b3280
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-07-29 05:15:15 +0530

    Update 037_ProductofArrayExceptSelf.cpp

--- 037_ProductofArrayExceptSelf.cpp
+++ 037_ProductofArrayExceptSelf.cpp
@@ -19,7 +19,7 @@ vector<int> productExceptSelf(vector<int>& nums){
 }
 
 //Brute Force
-vector<int> productExceptSelf(vector<int>& nums){
+vector<int> productExceptSelf1(vector<int>& nums){
 	int n=nums.size();
 	vector<int> ans(n,1);
 	for(int i=0; i<n; i++){
@@ -30,6 +30,45 @@ vector<int> productExceptSelf(vector<int>& nums){
 	return ans;
 }
 
+
+//Optimal Running Time & Space Complexity=O(n)
+vector<int> productExceptSelf2(vector<int>& nums){
+	int n=nums.size();
+	vector<int>	ans(n);
+	vector<int> prefix(n,1);
+	vector<int> suffix(n,1);
+	
+	for(int i=1; i<n; i++){
+		prefix[i]=prefix[i-1]*nums[i-1];
+	}
+	
+	for(int i=n-1; i>=0; i--){
+		prefix[i]=prefix[i+1]*nums[i+1];
+	}
+	
+	for(int i=0; i<n; i++){
+		ans[i]=suffix[i]*prefix[i];
+	}
+	return ans;
+}
+
+//Optimal Running Time=O(n) & Space Complexity=O(1)	if we not consider vector ans
+vector<int> productExceptSelf2(vector<int>& nums){
+	int n=nums.size();
+	vector<int>	ans(n,1);
+	int suffix=1;
+	//Prefix
+	for(int i=1; i<n; i++){
+		ans[i]=nums[i-1]*ans[i-1];
+	}
+	//Suffix
+	for(int i=n-2; i>=0; i--){
+		suffix*=nums[i+1];
+		ans[i]*=suffix;
+	}
+	
+	return ans;
+}
 
 int main(){
 	
