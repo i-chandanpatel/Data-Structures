d4a7085a22470bb596e4a3677f20d55c7ae0984c
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-07-30 05:32:33 +0530

    Update and rename SearchInRotatedArray.cpp to 039_SearchInRotatedArray.cpp

--- 039_SearchInRotatedArray.cpp
+++ 039_SearchInRotatedArray.cpp
@@ -1,20 +1,20 @@
 #include<iostream>
 using namespace std;
 
-int searchInRotatedArray(int arr[], int n, int s){
+int searchInRotatedArray(int arr[], int n, int search){
 	int start=0, end=n-1, mid;
 	while(start<=end){
 		mid=start+(end-start)/2;
-		if(arr[mid]==s){
+		if(arr[mid]==search){
 			return mid;
 		}else if(arr[mid]>=arr[0]){		//Left Side Sorted	{4,5,6,7}
-			if(arr[start]<=s && arr[mid]>s)
+			if(arr[start]<=search && arr[mid]>search)
 				end=mid-1;
 			else
 				start=mid+1;
 		}
 		else{		//Right Side Sorted	{1,2,3}
-			if(arr[mid]<s && arr[end]>=s)
+			if(arr[mid]<search && arr[end]>=search)
 				start=mid+1;
 			else
 				end=mid-1;
