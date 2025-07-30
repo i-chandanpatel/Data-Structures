957a9901b82ce016696a90a437811c1539062b3d
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-07-30 05:58:38 +0530

    Update 003_DecimaltoBinary.cpp

--- 003_DecimaltoBinary.cpp
+++ 003_DecimaltoBinary.cpp
@@ -2,34 +2,49 @@
 #include<cmath>
 using namespace std;
 
-//Approach 1: Bit method
+// Approach 1: Bit method
 int decimaltoBinary(int n){
-	int ans=0, i=0;
-	while(n!=0){
-		int bit=n&1;	//Extracts least significant bit of n i.e 6=>110  => 0  => 1  => 1
-		ans=(bit*pow(10,i))+ans;
-		n=n>>1;		//Shifts n right by one bit (/2) i.e 6/2=3 => 3/2=1 => 1/2=0
+	int ans = 0, i = 0;
+
+	while(n != 0){
+		// Extract the least significant bit (LSB)
+		int bit = n & 1;  // Example: 6 => 110 → LSB is 0, then 1, then 1
+
+		// Place the bit at the correct position in the binary number
+		// We multiply by 10^i because we’re forming the binary number in base 10 (e.g., 110)
+		ans = (bit * pow(10, i)) + ans;
+
+		// Shift the number to the right by 1 bit (i.e., divide by 2)
+		n = n >> 1;
 		i++;
 	}
 	return ans;
 }
 
 
-//Approach 2: Brute Force
-//int decimaltoBinary(int n){
-//	int ans=0, i=0;
-//	while(n > 0) {
-//        ans += (n % 2) * pow(10,i);
-//        n /= 2;
-//        i++;
-//    }
-//    return ans;
-//}
+// Approach 2: Brute Force
+// int decimaltoBinary(int n){
+// 	int ans = 0, i = 0;
+
+// 	while(n > 0) {
+// 		// Get the remainder when divided by 2 (either 0 or 1)
+// 		int rem = n % 2;
+
+// 		// Place the digit in the correct place value (1s, 10s, 100s...)
+// 		ans += rem * pow(10, i);
+
+// 		// Reduce the number by dividing by 2
+// 		n /= 2;
+// 		i++;
+// 	}
+// 	return ans;
+// }
 
 int main(){
 	int n;
-	cout<<"Enter Decimal number: ";
-	cin>>n;
-	cout<<"Binary value: "<<decimaltoBinary(n);
+	cout << "Enter Decimal number: ";
+	cin >> n;
+
+	cout << "Binary value: " << decimaltoBinary(n);
 	return 0;
 }
