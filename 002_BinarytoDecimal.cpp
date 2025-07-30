dc733b0e5f62393ba1d6b90016290d8f96d25527
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-07-30 05:54:17 +0530

    Update 002_BinarytoDecimal.cpp

--- 002_BinarytoDecimal.cpp
+++ 002_BinarytoDecimal.cpp
@@ -1,24 +1,33 @@
 #include<iostream>
-#include<cmath>
+#include<cmath>     // For using the power function pow()
 using namespace std;
 
+// Function to convert binary number to decimal
 int binarytoDecimal(int n){
 	int digit, ans=0, i=0;
-	while(n!=0){
-		digit=n%10;
-		if(digit==1){
-			ans=ans+pow(2,i);
+
+	// Run the loop until all digits are processed
+	while(n != 0){
+		digit = n % 10;      // Get the last digit (either 0 or 1)
+		
+		if(digit == 1){
+			// If digit is 1, add 2^i to the answer
+			ans = ans + pow(2, i);
 		}
-		n=n/10;
+		
+		n = n / 10;   // Remove the last digit
 		i++;
 	}
+	
 	return ans;
 }
 
 int main(){
 	int n;
-	cout<<"Enter Binary number: ";
-	cin>>n;
-	cout<<"Decimal value: "<<binarytoDecimal(n);
+	cout << "Enter Binary number: ";
+	cin >> n;
+
+	cout << "Decimal value: " << binarytoDecimal(n);
+	
 	return 0;
 }
