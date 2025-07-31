15b68c837c4b7af0c5015216e21943f7e1a2dc47
Chandan Patel <152427076+i-chandanpatel@users.noreply.github.com>
2025-07-31 13:09:38 +0530

    Update 040_PeakIndexinMountainArray.cpp

--- 040_PeakIndexinMountainArray.cpp
+++ 040_PeakIndexinMountainArray.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 int peakIndex(int arr[], int n){
-	int start=0, end=n-1, mid;
-	
-	while(start<=end){
-		mid=end+(start-end)/2;
-		if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1])
-			return mid;
-		else if(arr[mid]>arr[mid-1])
-			start=mid-1;
-		else
-			end=mid-1;
-	}
+    int start = 0, end = n - 1, mid;
+
+    while(start < end){
+        mid = start + (end - start) / 2;
+
+        if(arr[mid] < arr[mid + 1]){
+            // You're in the increasing part
+            start = mid + 1;
+        } else {
+            // You're in the decreasing part, maybe this is the peak
+            end = mid;
+        }
+    }
+    return start; // or return end; both are same when loop ends
 }
 
 int main(){
